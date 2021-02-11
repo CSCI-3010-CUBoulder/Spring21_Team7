0edb7ee2daba9c865bae902342f357373a173798
Noahw19 <58636123+Noahw19@users.noreply.github.com>
2021-02-11 10:23:45 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -11,3 +11,23 @@ TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( Factorial(3) == 6 );
     REQUIRE( Factorial(10) == 3628800 );
 }
+
+int CheckIfAbove10(int number) 
+{
+    if(number > 10)
+    {
+      return 100;
+    }
+    else
+    {
+      return 0;
+    }
+     
+}
+
+TEST_CASE( "CheckIfAbove10 are computed" ) {
+    REQUIRE( CheckIfAbove10(10) == 0 );
+    REQUIRE( CheckIfAbove10(19) == 100 );
+    REQUIRE( CheckIfAbove10(3) == 0 );
+    REQUIRE( CheckIfAbove10(100) == 100);
+}
