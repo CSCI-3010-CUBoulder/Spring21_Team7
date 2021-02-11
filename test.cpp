3dce48a9852f9f4a8cf288fedb9eb60f25df7617
Noahw19 <58636123+Noahw19@users.noreply.github.com>
2021-02-11 10:24:34 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,28 +1,11 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
-}
-
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( Factorial(1) == 1 );
     REQUIRE( Factorial(2) == 2 );
     REQUIRE( Factorial(3) == 6 );
     REQUIRE( Factorial(10) == 3628800 );
-}
-
-int CheckIfAbove10(int number) 
-{
-    if(number > 10)
-    {
-      return 100;
-    }
-    else
-    {
-      return 0;
-    }
-     
 }
 
 TEST_CASE( "CheckIfAbove10 are computed" ) {
